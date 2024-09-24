bd0f9f717b32f671098dcd6cb9c65d4a24dc0358
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2024-09-24 12:44:06 +0200

    Export button styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,6 +3,7 @@ import { Button as RButton, type ButtonProps } from "react-aria-components"
 
 import Styles from "./styles.module.css"
 
+export const buttonStyles = Styles.button
 export const Button: FC<ButtonProps> = ({ children, ...props }) => {
   return (
     <RButton

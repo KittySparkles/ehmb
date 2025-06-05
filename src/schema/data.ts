fcc9783aaf4a4170282feb598c9484994b14ebb7
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-06-05 13:05:42 +0200

    Add Scythe

--- src/schema/data.ts
+++ src/schema/data.ts
@@ -3,6 +3,7 @@ import { DOUBLE_BLADE_SCHEMA } from "./DOUBLE_BLADE"
 import { DUAL_PISTOL_SCHEMA } from "./DUAL_PISTOL"
 import { MAGIC_STAFF_SCHEMA } from "./MAGIC_STAFF"
 import { RIFLE_SCHEMA } from "./RIFLE"
+import { SCYTHE_SCHEMA } from "./SCYTHE"
 import { SHIELD_SCHEMA } from "./SHIELD"
 import { STONE_ORB_SCHEMA } from "./STONE_ORB"
 import { TWO_HANDED_SCHEMA } from "./TWO_HANDED"
@@ -28,6 +29,7 @@ export const MASTERIES = [
     schema: MAGIC_STAFF_SCHEMA,
   },
   { name: "Rifle", slug: "rifle", id: "ri" as const, schema: RIFLE_SCHEMA },
+  { name: "Scythe", slug: "scythe", id: "sh" as const, schema: SCYTHE_SCHEMA },
   { name: "Shield", slug: "shield", id: "sh" as const, schema: SHIELD_SCHEMA },
   {
     name: "Stone Orb",

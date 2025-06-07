4df6aaebf72d6adcd0aaa9c737d625e4493f1877
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-06-07 20:03:48 +0200

    Remove recommended builds

--- src/components/RecommendedBuilds/data.ts
+++ src/components/RecommendedBuilds/data.ts
@@ -9,14 +9,7 @@ type RecommendedBuild = {
 
 const BOW: RecommendedBuild[] = [];
 
-const DOUBLE_BLADE: RecommendedBuild[] = [
-  {
-    name: 'Single DPS',
-    author: 'Keylir',
-    hash: 'dbe5f5n1t5z5A5F5',
-    notes: 'Final 3 points flexbile, negligible gains anywhere you put them. I usually just do Death for All to make the build a one size fits all type. The other two I usually drop into movespeed, but Shadow Colossus is an option if you just like the ability.'
-  }
-];
+const DOUBLE_BLADE: RecommendedBuild[] = [];
 
 const DUAL_PISTOL: RecommendedBuild[] = [];
 
@@ -26,29 +19,7 @@ const RIFLE: RecommendedBuild[] = [];
 
 const SCYTHE: RecommendedBuild[] = [];
 
-const SHIELD: RecommendedBuild[] = [
-  {
-    name: "Single-target core",
-    author: "Kitty",
-    hash: "shb5c5q1s5t5y5D1E1F1",
-    notes:
-      "Whether you have the base Shield or Oathkeeper doesn’t really matter too much.\nIf you have the Braveheart pet at celestial 1 rank, you can skip Bastion’s Resilience.\nYou can spend some points in Sharpened Instincts to be crit-capped.\nYou can decide whether you want Defensive Stance + Last Resort.\nThe remaining points can be spent across Critical Edge, Vitality and Swift Protector.",
-  },
-  {
-    name: "Multi-target core",
-    author: "Kitty",
-    hash: "shc5p5r1s5C1E1",
-    notes:
-      "Whether you have the base Shield or Oathkeeper doesn’t really matter too much.\nIf you have the Braveheart pet at celestial 1 rank, you can skip Bastion’s Resilience.\nYou can spend some points in Sharpened Instincts to be crit-capped.\nYou can decide whether you want Defensive Stance + Last Resort.\nThe remaining points can be spent across Ironclad, Vitality, Radiant Strike and Swift Protector.",
-  },
-  {
-    name: "Hybrid core",
-    author: "Kitty",
-    hash: "shc5q1r1s5t5C1D1E1",
-    notes:
-      "Whether you have the base Shield or Oathkeeper doesn’t really matter too much.\nIf you have the Braveheart pet at celestial 1 rank, you can skip Bastion’s Resilience.\nYou can spend some points in Sharpened Instincts to be crit-capped.\nYou can decide whether you want Defensive Stance + Last Resort.\nThe remaining points can be spent across Vitality and Swift Protector.",
-  },
-];
+const SHIELD: RecommendedBuild[] = [];
 
 const STONE_ORB: RecommendedBuild[] = [];
 

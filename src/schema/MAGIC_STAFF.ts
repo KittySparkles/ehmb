16b0a69c107e5ff2dbfd83dc11709e1c1f180880
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-06-17 17:39:39 +0200

    Move staff descriptions to translations

--- src/schema/MAGIC_STAFF.ts
+++ src/schema/MAGIC_STAFF.ts
@@ -3,167 +3,240 @@ import type { RawSkill } from "../types"
 export const MAGIC_STAFF_SCHEMA = [
   {
     id: 115,
-    description: "Increase *Damage* by *5/10/15/20/25%*.",
+    variables: {
+      statName0: { type: "translation", value: "Stat_AllDamage" },
+      statPower0: { type: "raw", value: "5/10/15/20/25%" },
+    },
     max: 5,
     position: [0, 0],
   },
   {
     id: 116,
-    description: "Increase *Attack Speed* by *2/4/6/8/10%*.",
+    variables: {
+      statName0: { type: "translation", value: "Stat_AttackSpeed" },
+      statPower0: { type: "raw", value: "2/4/6/8/10%" },
+    },
     max: 5,
     position: [0, 1],
   },
   {
     id: 119,
-    description:
-      "Grant a permanent flame shield increasing *Armor* by *15/30/45/60/75* and deals *X* damage back to attackers.",
+    variables: {
+      statName0: { type: "translation", value: "Stat_Armor" },
+      effectCount0: { type: "raw", value: "15/30/45/60/75" },
+      effectDamage0: { type: "raw", value: "X" },
+    },
     max: 5,
     position: [0, 2],
   },
   {
     id: 117,
-    description:
-      "Increase *Max HP* by *20/40/60/80/100* and *Movement Speed* by *1/2/3/4/5%*.",
+    variables: {
+      statName0: { type: "translation", value: "Stat_MaxHp" },
+      statPower0: { type: "raw", value: "20/40/60/80/100" },
+      statName1: { type: "translation", value: "Stat_MovementSpeed" },
+      statPower1: { type: "raw", value: "1/2/3/4/5%" },
+    },
     max: 5,
     position: [0, 3],
   },
   {
     id: 118,
-    description: "Increase *Movement Speed* by *2/4/6/8/10%*.",
+    variables: {
+      statName0: { type: "translation", value: "Stat_MovementSpeed" },
+      statPower0: { type: "raw", value: "6/12/18/24/30%" },
+    },
     max: 5,
     position: [1, 1],
     requires: 116,
   },
   {
     id: 122,
-    description:
-      "Taking damage has a *10/20/30/40/50%* chance to increase *Movement Speed* by *7/15/22.5/30/37.5%* and dealing *X* damage to nearby enemies every *0.5s* for *5s*.",
+    variables: {
+      effectChance0: { type: "raw", value: "10/20/30/40/50%" },
+      statName0: { type: "translation", value: "Stat_MovementSpeed" },
+      effectCount0: { type: "raw", value: "7/15/22.5/30/37.5%" },
+      effectDamage0: { type: "raw", value: "X" },
+      effectEveryX0: { type: "raw", value: "0.5" },
+      effectDuration0: { type: "raw", value: "5" },
+    },
     max: 5,
     position: [1, 2],
     requires: 119,
   },
   {
     id: 120,
-    description:
-      "Your attacks have a *2.5/5/7.5/10/12.5%* chance to conjure *1/2/3/4/5* ice lance(s) dealing *X* damage to a random active target.",
+    variables: {
+      effectChance0: { type: "raw", value: "2.5/5/7.5/10/12.5%" },
+      effectCount0: { type: "raw", value: "1/2/3/4/5" },
+      effectDamage0: { type: "raw", value: "X" },
+    },
     max: 5,
     position: [1, 3],
   },
   {
     id: 121,
-    description:
-      "Increase *Crit Chance* by *2/4/6/8/10%* and *Crit Damage* by *5/10/15/20/25%*.",
+    variables: {
+      statName0: { type: "translation", value: "Stat_CriticalChance" },
+      statPower0: { type: "raw", value: "2/4/6/8/10%" },
+      statName1: { type: "translation", value: "Stat_CriticalDamage" },
+      statPower1: { type: "raw", value: "5/10/15/20/25%" },
+    },
     max: 5,
     position: [2, 0],
     requires: 115,
   },
   {
     id: 131,
-    description:
-      "Increase *Attack Speed* by *3/6/9/12/15%*, *Leech* by *2/3/4/8/10* and *HP Regen* by *40/80/120/160/200*.",
+    variables: {
+      statName0: { type: "translation", value: "Stat_AttackSpeed" },
+      statPower0: { type: "raw", value: "3/6/9/12/15%" },
+      statName1: { type: "translation", value: "Stat_LifeSteal" },
+      statPower1: { type: "raw", value: "2/3/4/8/10" },
+      statName2: { type: "translation", value: "Stat_HpRegen" },
+      statPower2: { type: "raw", value: "40/80/120/160/200" },
+    },
     max: 5,
     position: [2, 2],
   },
   {
     id: 123,
-    description:
-      "*Ice Lances* now shatter on hit, dealing *50/80/110/140/170%* of original damage in a small radius.",
+    variables: {
+      effectPower0: { type: "raw", value: "50/80/110/140/170%" },
+    },
     max: 5,
     position: [2, 3],
     requires: 120,
   },
   {
     id: 124,
-    description:
-      "Increase *Damage* by *5/10/15/20/25%* and *Max HP* by *30/60/90/120/150*.",
+    variables: {
+      statName0: { type: "translation", value: "Stat_AllDamage" },
+      statPower0: { type: "raw", value: "5/10/15/20/25%" },
+      statName1: { type: "translation", value: "Stat_MaxHp" },
+      statPower1: { type: "raw", value: "30/60/90/120/150" },
+    },
     max: 5,
     position: [3, 0],
     requires: 121,
   },
   {
     id: 125,
-    description:
-      "Every *20th* attack calls down a lightning storm, dealing *X* damage every *0.5s* for *3s*.",
+    variables: {
+      attackCount0: { type: "raw", value: "20", highlight: false },
+      effectDamage0: { type: "raw", value: "X" },
+      effectEveryX0: { type: "raw", value: "0.5" },
+      effectDuration0: { type: "raw", value: "3" },
+    },
     max: 5,
     position: [3, 1],
   },
   {
     id: 126,
-    description:
-      "Every *12th* attack launches a frozen orb dealing *X* damage and slowing enemies by *20%* for *4s*.",
+    variables: {
+      attackCount0: { type: "raw", value: "12", highlight: false },
+      effectDamage0: { type: "raw", value: "X" },
+      effectDuration0: { type: "raw", value: "4" },
+    },
     max: 5,
     position: [3, 3],
   },
   {
     id: 127,
-    description:
-      "When *Thunderstorm* expires, it releases a static energy blast, dealing *X* extra damage to all enemies within range.",
+    variables: {
+      effectDamage0: { type: "raw", value: "X" },
+    },
     max: 5,
     position: [4, 1],
     requires: 125,
   },
   {
     id: 128,
-    description:
-      "Your attacks ignite enemies, dealing *X* damage every *0.49s* for *1.8/2/2.2/2.4/2.6s*.",
+    variables: {
+      effectDamage0: { type: "raw", value: "X" },
+      effectEveryX0: { type: "raw", value: "0.49" },
+      effectDuration0: { type: "raw", value: "1.8/2/2.2/2.4/2.6" },
+    },
     max: 5,
     position: [4, 2],
   },
   {
     id: 129,
-    description:
-      "When *Frozen Orbs* expire, they explode dealing *X* damage to nearby enemies and freezing them for *1.5/1.7/1.8/2/2.1s*.",
+    variables: {
+      effectDamage0: { type: "raw", value: "X" },
+      effectDuration0: { type: "raw", value: "1.5/1.7/1.8/2/2.1" },
+    },
     max: 5,
     position: [4, 3],
     requires: 126,
   },
   {
     id: 130,
-    description: "Increase *Attack Target* by *1*.",
+    variables: {
+      statName0: { type: "translation", value: "Stat_ExtraAttackTargets" },
+      statPower0: { type: "raw", value: "1" },
+    },
     max: 1,
     position: [5, 0],
     requires: 124,
   },
   {
     id: 201,
-    description: "Increase *Burn Damage (DoT)* by *5/10/15/20/25%*.",
+    variables: {
+      statName0: {
+        type: "translation",
+        value: "Stat_FireDotDamage",
+        highlight: false,
+      },
+      statPower0: { type: "raw", value: "5/10/15/20/25%" },
+    },
     max: 5,
     position: [5, 2],
     requires: 128,
   },
   {
     id: 132,
-    description:
-      "Every *12th* attack calls down a meteor, dealing *X* damage at its impact center with reduced damage falloff.",
+    variables: {
+      attackCount0: { type: "raw", value: "12", highlight: false },
+      effectDamage0: { type: "raw", value: "X" },
+    },
     max: 5,
     position: [6, 2],
   },
   {
     id: 133,
-    description:
-      "Every *30/27/24/21/18s*, summon a blizzard that travels towards nearby enemies, dealing *X* damage every *0.5s* for *5s* in a *1/2/3/4/5m* area.",
+    variables: {
+      effectCooldown0: { type: "raw", value: "30/27/24/21/18" },
+      effectDamage0: { type: "raw", value: "X" },
+      effectEveryX0: { type: "raw", value: "0.5" },
+      effectDuration0: { type: "raw", value: "5" },
+      effectCount0: { type: "raw", value: "1/2/3/4/5", highlight: false },
+    },
     max: 5,
     position: [6, 3],
   },
   {
     id: 134,
-    description: "Your basic attacks now *ricochet* once.",
+    variables: {},
     max: 1,
     position: [7, 0],
     requires: 130,
   },
   {
     id: 136,
-    description:
-      "*Meteor* now splits into *6* fragments, covering a larger area, with each fragment dealing *40%* of the original meteor’s damage.",
+    variables: {
+      effectCount0: { type: "raw", value: "6", highlight: false },
+      effectPower0: { type: "raw", value: "40%" },
+    },
     max: 1,
     position: [7, 2],
     requires: 132,
   },
   {
     id: 137,
-    description:
-      "*Blizzard* is now permanent and follows you, but deals *75%* of its original damage.",
+    variables: {
+      effectPower0: { type: "raw", value: "75%" },
+    },
     max: 1,
     position: [7, 3],
     requires: 133,
